aab0a71bbde3d35d29b293d023c1bb11658c14c0
leeweixin0917 <leexin2019@126.com>
2020-10-29 00:43:42 +0800

    react生命周期

--- src/Xiaojiejie.js
+++ src/Xiaojiejie.js
@@ -1,81 +1,113 @@
-import React,{Component,Fragment} from 'react'
-import './style.css'
-import XiaojiejieItem from './XiaojiejieItem';
-class Xiaojiejie extends Component{
-    // 在某一时刻，可以自动执行的函数
-    constructor(props){
-        super(props)
-        this.state={
-            inputValue:'',
-            list:['头部按摩','精油推背']
+import React, { Component, Fragment } from "react";
+import axios from "axios";
+import "./style.css";
+import XiaojiejieItem from "./XiaojiejieItem";
+class Xiaojiejie extends Component {
+  // 在某一时刻，可以自动执行的函数
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: "",
+      list: ["头部按摩", "精油推背"],
+    };
+  }
+  //   componentWillMount() {
+  //     console.log("componentWillMount---------组件将要挂载到页面的时刻");
+  //   }
+  //   componentDidMount() {
+  //     console.log("componentWillMount---------组件挂载完成的时刻");
+  //   }
+  //   shouldComponentUpdate() {
+  //     console.log(`shouldComponentUpdate`);
+  //     return true;
+  //     // return false; // 就不会往下面执行了。
+  //   }
+  //   componentWillUpdate() {
+  //     console.log(`ComponentWillUpdate`);
+  //     return true;
+  //   }
+  //   componentDidUpdate() {
+  //     console.log(`componentDidUpdate`);
+  //   }
+
+  componentDidMount() {
+    axios
+      .post("https://web-api.juejin.im/v3/web/wbbr/bgeda")
+      .then((res) => {
+        console.log("axio获取数据成功:" + JSON.stringify(res));
+      })
+      .catch((error) => {
+        console.log(`axios获取数据失败:` + error);
+      });
+  }
+  render() {
+    // console.log("render----组件挂载中");
+    return (
+      <Fragment>
+        {/* 第一次注释 (按住Ctrl+/)*/}
+        {
+          //第一次注释 (单行注释)
         }
-    }
-    componentWillMount() {
-        console.log('componentWillMount---------组件将要挂载到页面的时刻')
-    }
-    componentDidMount() {
-        console.log('componentWillMount---------组件挂载完成的时刻')
-    }
-    render(){
-        console.log('render----组件挂载中')
-        return (
-            <Fragment>
-                {/* 第一次注释 (按住Ctrl+/)*/}
-                {
-                //第一次注释 (单行注释)
-                }
-                <div>
-                    <label htmlFor="jspang">增加服务：</label>
-                    <input 
-                        id="jspang" 
-                        className="input" 
-                        value={this.state.inputValue} 
-                        onChange={this.inputChange.bind(this)}
-                        ref={(input)=>{this.input=input}}
-                    />
-                    <button onClick={this.addList.bind(this)}>增加服务</button>
-                </div>
-                <ul ref={(ul)=>{this.ul=ul}}>
-                    {
-                        this.state.list.map((item,index)=>{
-                        return (
-                            <XiaojiejieItem 
-                            key={index+item} 
-                            content={item} 
-                            index={index} 
-                            deleteItem={this.deleteItem.bind(this)}/>
-                            )
-                        })
-                    }
-                </ul>
-            </Fragment>
-        )
-    }
-    //inputChange(e){
-        inputChange(){
-        // console.log(this)
-        // this.state.inputValue=e.target.value // 错误书写
-        this.setState({
-            inputValue: this.input.value
-        })
-    }
-    addList(){
-        // setState是异步的
-        this.setState({
-            list:[...this.state.list,this.state.inputValue],
-            inputValue:''
-        },()=>{
-            console.log(this.ul.querySelectorAll("li").length)
-        })
-        console.log(this.ul.querySelectorAll("li").length)
-    }
-    deleteList(index){
-        console.log(index)
-        let list = this.state.list
-        list.splice(index,1)
-        this.setState({
-            list:list
-        })
-    }
+        <div>
+          <label htmlFor="jspang">增加服务：</label>
+          <input
+            id="jspang"
+            className="input"
+            value={this.state.inputValue}
+            onChange={this.inputChange.bind(this)}
+            ref={(input) => {
+              this.input = input;
+            }}
+          />
+          <button onClick={this.addList.bind(this)}>增加服务</button>
+        </div>
+        <ul
+          ref={(ul) => {
+            this.ul = ul;
+          }}
+        >
+          {this.state.list.map((item, index) => {
+            return (
+              <XiaojiejieItem
+                key={index + item}
+                content={item}
+                index={index}
+                deleteItem={this.deleteItem.bind(this)}
+              />
+            );
+          })}
+        </ul>
+      </Fragment>
+    );
+  }
+  //inputChange(e){
+  inputChange() {
+    // console.log(this)
+    // this.state.inputValue=e.target.value // 错误书写
+    this.setState({
+      inputValue: this.input.value,
+    });
+  }
+  addList() {
+    // setState是异步的
+    this.setState(
+      {
+        list: [...this.state.list, this.state.inputValue],
+        inputValue: "",
+      },
+      () => {
+        console.log(this.ul.querySelectorAll("li").length);
+      }
+    );
+    console.log(this.ul.querySelectorAll("li").length);
+  }
+  deleteItem(index) {
+    console.log(index);
+    let list = this.state.list;
+    list.splice(index, 1);
+    this.setState({
+      list: list,
+    });
+  }
 }
-export default Xiaojiejie
+export default Xiaojiejie;
--- src/XiaojiejieItem.js
+++ src/XiaojiejieItem.js
@@ -1,30 +1,47 @@
-import React, { Component } from 'react';
-// import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import propTypes from "prop-types";
 
 class XiaojiejieItem extends Component {
-    constructor(props){
-        super(props)
-        this.handleClick = this.handleClick.bind(this)
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  // 组件第一次存在于dom中，函数不会被执行
+  // 如果已经存在于dom中，函数才会被执行
+  //   componentWillReceiveProps() {
+  //     console.log(`child---componentWillReceiveProps`);
+  //   }
+  //   componentWillUnmount() {
+  //     console.log(`child---componentWillUnmount`);
+  //   }
+  // 避免多次渲染render
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextProps.content !== this.props.content) {
+      return true;
+    } else {
+      return false;
     }
-    render() {
-        return (
-            <li onClick={this.handleClick}>
-              {this.props.avname}为你服务--{this.props.content}
-            </li>
-        );
-    }
-    handleClick() {
-        this.props.deleteItem(this.props.index)
-    }
+  }
+  render() {
+    console.log(`child-render`);
+    return (
+      <li onClick={this.handleClick}>
+        {this.props.avname}为你服务--{this.props.content}
+      </li>
+    );
+  }
+  handleClick() {
+    this.props.deleteItem(this.props.index);
+  }
 }
 
-// XiaojiejieItem.PropTypes = {
-//     avname:PropTypes.string.isRequired,
-//     content:PropTypes.string,
-//     index:PropTypes.number,
-//     deleteItem:PropTypes.func
-// }
-// XiaojiejieItem.defaultProps = {
-//     avname:'哈哈'
-// }
+XiaojiejieItem.propTypes = {
+  avname: propTypes.string.isRequired,
+  content: propTypes.string,
+  index: propTypes.number,
+  deleteItem: propTypes.func,
+};
+XiaojiejieItem.defaultProps = {
+  avname: "哈哈",
+};
 export default XiaojiejieItem;
